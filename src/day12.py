d25137b0a7ed549aa81faac713701a437cbd989b
Guru Somasundaram <9uruprasad@gmail.com>
2021-12-14 00:24:12 -0600

    day12 done

--- src/day12.py
+++ src/day12.py
@@ -3,8 +3,9 @@ Solution for day 12
 Author: 9uru
 12/13/2021
 '''
-
+from collections import defaultdict, Counter
 from typing import List
+from src import day10
 
 class CaveGraph:
     '''
@@ -16,24 +17,16 @@ class CaveGraph:
         '''
         add to graph bidirectionally when applicable
         '''
-        if start in self.graph:
-            self.graph[start].add(end)
-        else:
-            self.graph[start] = {end}
+        if end == "start":
+            start, end = end, start
 
-        if start != "start" and end != "end":
-            if end in self.graph:
-                self.graph[end].add(start)
-            else:
-                self.graph[end] = {start}
-
-        keywords = {"start", "end"}
+        self.graph[start].add(end)
+        self.graph[end].add(start)
         for word in [start, end]:
-            if word not in keywords:
-                if word.lower() == word:
-                    self.small_caves.add(word)
-                else:
-                    self.big_caves.add(word)
+            if word.islower():
+                self.small_caves.add(word)
+            else:
+                self.big_caves.add(word)
 
 
     def __init__(self, connects: List[str]):
@@ -42,15 +35,20 @@ class CaveGraph:
         '''
         self.small_caves = set()
         self.big_caves = set()
-        self.graph = {}
+        self.graph = defaultdict(set)
+        self.keywords = {"start", "end"}
         # cache for storing paths
         self.paths = {}
+        self.paths2 = {}
 
         for connect in connects:
             start, end = connect.split("-")
             self.add_to_graph(start, end)
 
         self.calculate_paths()
+        self.calculate_paths2('start', 'end', set(), set(), 0)
+
+
 
     def calculate_paths(self, start='start', end='end', visited_small=set()):
         '''
@@ -58,6 +56,8 @@ class CaveGraph:
         while going through small caves only once
         '''
         key = start + "-" + end
+        if start in self.small_caves:
+            visited_small.add(start)
         if start == end:
             return [end]
 
@@ -66,9 +66,8 @@ class CaveGraph:
         if start in self.graph:
             for next_node in self.graph[start]:
                 cur_visited = visited_small.copy()
-
                 if next_node in self.small_caves:
-                    if next_node in visited_small:
+                    if next_node in cur_visited:
                         continue
                     else:
                         cur_visited.add(next_node)
@@ -76,3 +75,57 @@ class CaveGraph:
                     paths.append(start + "-" + next_path)
         self.paths[key] = paths
         return paths
+
+    def calculate_paths2(self, start='start', end='end', visited_once=None, visited_twice=None, count=0):
+        '''
+        Find all paths from start to end
+        while going through small caves only once
+        '''
+        key = start + "-" + end
+        if start in self.keywords:
+            visited_once.add(start)
+        if start == end:
+            return [end]
+        # print(self.keywords, self.small_caves)
+        # print(count, start, end, visited_once, visited_twice)
+        # if count > 50:
+        #     return
+        paths = []
+
+        if start in self.graph:
+            for next_node in self.graph[start]:
+                # print(next_node)
+                cur_visited_once = visited_once.copy()
+                cur_visited_twice = visited_twice.copy()
+                if next_node in self.keywords:
+                    if next_node in cur_visited_once:
+                        continue
+                    else:
+                        cur_visited_once.add(next_node)
+                elif next_node in self.small_caves:
+                    if cur_visited_twice and next_node in cur_visited_once:
+                        continue
+                    elif next_node in cur_visited_once:
+                        cur_visited_twice.add(next_node)
+                    else:
+                        cur_visited_once.add(next_node)
+
+                for next_path in self.calculate_paths2(next_node, end, cur_visited_once, cur_visited_twice, count + 1):
+                    paths.append(start + "-" + next_path)
+        self.paths2[key] = paths
+        return paths
+
+
+
+def main(paths_filename: str):
+    '''
+    main workflow
+    '''
+    connects = day10.parse_input(paths_filename)
+    cave_graph = CaveGraph(connects)
+    print(len(cave_graph.paths['start-end']))
+    print(len(cave_graph.paths2['start-end']))
+
+if __name__ == "__main__":
+    INPUT_FILENAME = "inputs/day12_input.txt"
+    main(INPUT_FILENAME)
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -5,7 +5,7 @@ Author: 9uru
 '''
 import json
 import pytest
-from src import day12
+from src import day12, day10
 
 @pytest.fixture(name="cave_graphs")
 def test_cave_graph(day12_test, day12_test_small):
@@ -14,22 +14,24 @@ def test_cave_graph(day12_test, day12_test_small):
     big caves and small caves
     '''
     cave_graph_small = day12.CaveGraph(day12_test_small)
-    assert cave_graph_small.small_caves == {'b'}
+    assert cave_graph_small.small_caves == {'b', 'start', 'end'}
     assert cave_graph_small.big_caves == {'A'}
     assert cave_graph_small.graph == {
         'start': {'A'},
-        'A': {'b', 'end'},
-        'b': {'A', 'end'}
+        'A': {'b', 'end', 'start'},
+        'b': {'A', 'end'},
+        'end': {'b', 'A'}
     }
     cave_graph = day12.CaveGraph(day12_test)
-    assert cave_graph.small_caves == {'b', 'c', 'd'}
+    assert cave_graph.small_caves == {'b', 'c', 'd', 'start', 'end'}
     assert cave_graph.big_caves == {'A'}
     assert cave_graph.graph == {
         'start': {'A', 'b'},
-        'A': {'c', 'b', 'end'},
-        'b': {'A', 'd', 'end'},
+        'A': {'c', 'b', 'end', 'start'},
+        'b': {'A', 'd', 'end', 'start'},
         'c': {'A'},
-        'd': {'b'}}
+        'd': {'b'},
+        'end': {'A', 'b'}}
     return cave_graph_small, cave_graph
 
 def test_calculate_paths(cave_graphs):
@@ -37,6 +39,26 @@ def test_calculate_paths(cave_graphs):
     Test calculation of paths
     '''
     cave_graph_small, cave_graph = cave_graphs
+    # print(cave_graph.paths)
     assert len(cave_graph_small.paths['start-end']) == 3
     assert len(cave_graph.paths['b-end']) == 3
     assert len(cave_graph.paths['start-end']) == 10
+    print(json.dumps(cave_graph.paths2['start-end'], indent=2))
+    assert len(cave_graph.paths2['start-end']) == 36
+
+
+def test_bigger():
+    '''
+    Test with bigger inputs
+    '''
+    connects = day10.parse_input("inputs/day12_input_short.txt")
+    # # print(connects)
+    cave_graph_big = day12.CaveGraph(connects)
+    # print(cave_graph_big.graph)
+    assert len(cave_graph_big.paths['start-end']) == 19
+    # pass
+    connects_bigger = day10.parse_input("inputs/day12_input_medium.txt")
+    # # print(connects)
+    cave_graph_bigger = day12.CaveGraph(connects_bigger)
+    # print(cave_graph_bigger.graph)
+    assert len(cave_graph_bigger.paths['start-end']) == 226
